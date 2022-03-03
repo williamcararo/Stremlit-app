d44eee634767bf705a816ac3b2b14650c4f1b8af
William Cararo <83188295+williamcararo@users.noreply.github.com>
2022-03-03 14:03:47 -0300

    Update pranchas.py

--- pranchas.py
+++ pranchas.py
@@ -7,7 +7,8 @@ import streamlit as st
 from streamlit.proto.RootContainer_pb2 import SIDEBAR
 from streamlit.proto.Slider_pb2 import Slider
 import joblib
-from PIL import Image
+#from PIL import Image
+from pillow import Image
 
 
 model = joblib.load('modelo_grid_papai.pkl')
